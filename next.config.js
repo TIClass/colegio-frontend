db2bda99e5f0e61441840ded23b6d3862c9261e3
Raúl Sepúlveda <raulsetron@gmail.com>
2022-11-12 17:22:17 -0300

    variables de entornos

--- next.config.js
+++ next.config.js
@@ -8,12 +8,12 @@ const nextConfig = {
   },
   env: {
     //API_URL: 'https://ticlass-staging.herokuapp.com/',
-  	CLIENT_ID: 'k42t26Dn5cNqLyv9Zt36ZTFBKJRF7C6Aq2aLp4FV',
-  	CLIENT_SECRET: 'TJ2eJvovunI9rJDNCTm1OILRDlC4GNXuj1HZgx0grdkYzntTIMUz9NdyqcgT9i87WfFl3a7eOhqxrXc9dZBC4mVgYxg062hOIJMkhblSPM78dJ1TDnylrljlwiuSOdjn',
-  	TOKEN_GENERIC_API: 'ed00f82cd64c7ed6f0f7d90116e01247fcaa3a6f',
-    API_URL: 'http://localhost:8000/',
-  	SITE_URL: 'https://www.ticlass.com',
-  	SITE_NAME: 'TIClass.com',
+  	CLIENT_ID: process.env.CLIENT_ID || 'k42t26Dn5cNqLyv9Zt36ZTFBKJRF7C6Aq2aLp4FV',
+  	CLIENT_SECRET: process.env.CLIENT_SECRET || 'TJ2eJvovunI9rJDNCTm1OILRDlC4GNXuj1HZgx0grdkYzntTIMUz9NdyqcgT9i87WfFl3a7eOhqxrXc9dZBC4mVgYxg062hOIJMkhblSPM78dJ1TDnylrljlwiuSOdjn',
+  	TOKEN_GENERIC_API: process.env.TOKEN_GENERIC_API || 'ed00f82cd64c7ed6f0f7d90116e01247fcaa3a6f',
+    API_URL: process.env.API_URL || 'http://localhost:8000/',
+  	SITE_URL: process.env.SITE_URL || 'https://www.ticlass.com',
+  	SITE_NAME: process.env.SITE_NAME || 'TIClass.com',
   },
 }
 
