f65472517a3a2710135aaf7f5f8f670bd0f0d71c
Raúl Sepúlveda <raulsetron@gmail.com>
2022-11-17 15:21:08 -0300

    arreglo footer

--- components/Footer.js
+++ components/Footer.js
@@ -11,19 +11,14 @@ const Footer = () => {
           <small className="d-block text-muted">
             Powered by
             <Link href="https://www.ticlass.com/">
-              <Image src="/logos/img/logo-ticlass.svg" alt="TIClass" width={70} height={50} className={'powered-logo-footer pt-4'} />
+              <Image src="/logos/img/logo-ticlass.svg" alt="TIClass" width={70} height={50} className={'powered-logo-footer pb-1 m-2'} />
             </Link>©2015-2023
           </small>
         </Col>
       </Container>
 
       <style global jsx>{`
-        .powered-logo-footer{
-          width: 70px;
-          height: 35px;
-          background-size: 70px 50px;
-          margin: auto;
-        }
+
 
       `}</style>
     </footer>
