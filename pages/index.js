9e77790310144b3684ed06ad33f35c186c505970
Raúl Sepúlveda <raulsetron@gmail.com>
2022-11-21 18:24:20 -0300

    url dinamic sitemaps

--- pages/index.js
+++ pages/index.js
@@ -144,7 +144,7 @@ export default function Home(props) {
         </Container>
       </section>
 
-      
+
 
       <style global jsx>{`
         .slide {
--- pages/sitemap.xml/index.js
+++ pages/sitemap.xml/index.js
@@ -1,11 +1,33 @@
 import { getServerSideSitemap } from 'next-sitemap';
 export const getServerSideProps = async (ctx) => {
-    const response = await fetch("https://api.spacexdata.com/v4/capsules");
-    const capsules = await response.json();
-    const fields = capsules.map(capsule => ({
-        loc: `https://www.capsules.com/capsule/${capsule.id}`,
-        lastmod: new Date().toISOString(),
-    }));
-    return getServerSideSitemap(ctx, fields);
+  const locationParts = ctx.req.headers.host.split('.');
+  const subdomain = locationParts[0]
+
+  const urlReferer = ctx.req.headers.referer
+  const urlHost = ctx.req.headers.host
+
+  const useToken = `Token ${process.env.TOKEN_GENERIC_API}`
+  const url = `${process.env.API_SEO_URL}api/v1/sitemapapi/?proyect_name=${subdomain}`
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': useToken
+    }
+  }
+
+  const res = await fetch(url, options)
+  const data = await res.json()
+
+  const fields = data.packs.map(item => ({
+      loc: `https://${urlHost}${item.url}`,
+      lastmod: item.lastmod,
+      changefreq: item.changefreq,
+      priority: item.priority
+  }));
+
+  return getServerSideSitemap(ctx, fields);
 };
 export default function Site() { }
